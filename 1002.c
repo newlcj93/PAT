534b1b500b6d4275331e37d110e4e36b28dabd77
newlcj93 <362981473@qq.com>
2016-05-11 11:26:05 +0800

    1002

--- 1002.c
+++ 1002.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-double s[12];
+double s[1010];
 int main(){
     double c;
-    int num,k,t=0,max=0;
+    int n,k,cnt=0,max=0;
     memset(s,0,sizeof(s));
     for (int i=0;i<2;i++){
-        scanf("%d",&num);
-        for(int j=0;j<num;j++){
+        scanf("%d",&n);
+        while(n--){
             scanf("%d%lf",&k,&c);
             s[k]+=c;
             if (k>max) max=k;
         }
     }
     for(int i=0;i<max+1;i++){
-        if(s[i]) t++;
+        if(s[i]) cnt++;
     }
-    printf("%d",t);
+    printf("%d",cnt);
     for(int i=max;i>=0;i--){
         if (s[i]){
             printf(" %d %.1lf",i,s[i]);
